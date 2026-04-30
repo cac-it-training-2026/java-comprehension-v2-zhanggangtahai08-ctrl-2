abce2eaa537ac1cf4cd361b81fff881c6e177c65
tnagaoka <tnagaoka@C010423420.zipangu.cac.co.jp>
2026-04-30 12:34:49 +0900

    q02_advanced/question2半分まで終了しました。
    良かった点
    ・リストの使い方が分かってきた
    改善点・備忘録
    ・アドバンスのchallenge２から再スタート

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/Member.java
@@ -0,0 +1,77 @@
+package q01_basic.question06;
+
+public class Member extends AbstMember {
+
+	//TODO ここから実装する
+	private String name;
+	private int age;
+	private int rank;
+	private int id;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String passward) {
+		this.password = passward;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Member() {
+	}
+
+	public Member(int id, String passward, String name, int age, int rank) {
+
+		this.id = id;
+		this.password = passward;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+	}
+
+	public void showMember() {
+		System.out.println("***MEMBER DATA***");
+		System.out.println("id:" + id);
+		System.out.println("passward:" + password);
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
+		System.out.println("*****************");
+	}
+
+	public void buyItem() {
+		System.out.println(name + "purchased the item at 50% off");
+	}
+}
---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question01/Member.java
@@ -0,0 +1,107 @@
+package q02_advanced.question01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+
+	private String name;
+	private int age;
+	private int rank;
+	private int id;
+	private String password;
+	private List<Coupon> coupons;
+
+	public List<Coupon> getCoupons() {
+		return coupons;
+	}
+
+	public void setCoupons(List<Coupon> coupons) {
+		this.coupons = coupons;
+	}
+
+	/**
+	 * @param coupons
+	 */
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + ", rank=" + rank + ", id=" + id + ", password=" + password
+				+ ", coupons=" + coupons + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String passward) {
+		this.password = passward;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Member() {
+	}
+
+	public Member(int id, String passward, String name, int age, int rank) {
+
+		this.id = id;
+		this.password = passward;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+		this.coupons = new ArrayList<>();
+		Coupon coupon1 = new Coupon(1, 0.5, "最初の特典");
+		Coupon coupon2 = new Coupon(2, 0.25, "今月の特典");
+		coupons.add(coupon1);
+		coupons.add(coupon2);
+
+	}
+
+	public void showMember() {
+
+		System.out.println(this.toString());
+		//		System.out.println("***MEMBER DATA***");
+		//		System.out.println("id:" + id);
+		//		System.out.println("passward:" + password);
+		//		System.out.println("name:" + name);
+		//		System.out.println("age:" + age);
+		//		System.out.println("rank:" + rank);
+		//		System.out.println("*****************");
+	}
+
+	/**
+	 * @param coupons
+	 */
+
+}
